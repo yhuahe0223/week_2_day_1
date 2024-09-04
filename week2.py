fed093ba5b88013e8ca036e7512fe6620ea4f929
He <yhuahe@cps.k12.il.us>
2024-09-04 15:00:09 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,9 @@
+#to ask their the day
+day= input ("What day is it? ")
+print(" the day is ", day )
+#their favortie holiday 
+favHoliday = input ("What is your favorite holday?")
+print("Your favorite holday is", favHoliday)
+#prints it out in a whole sentence
+print ("Today is is", day,
+        " and your favorite holiday is ", favHoliday)
