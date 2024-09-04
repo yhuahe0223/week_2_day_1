29179a457d6235ee4a66e3aac1dc4b402ecb09ce
He <yhuahe@cps.k12.il.us>
2024-09-04 15:06:25 -0500

    string slicing

--- week2.py
+++ week2.py
@@ -1,9 +1,13 @@
-#to ask their the day
-day= input ("What day is it? ")
-print(" the day is ", day )
-#their favortie holiday 
-favHoliday = input ("What is your favorite holday?")
-print("Your favorite holday is", favHoliday)
-#prints it out in a whole sentence
-print ("Today is is", day,
-        " and your favorite holiday is ", favHoliday)
+# #to ask their the day
+# day= input ("What day is it? ")
+# print(" the day is ", day )
+# #their favortie holiday 
+# favHoliday = input ("What is your favorite holday?")
+# print("Your favorite holday is", favHoliday)
+# #prints it out in a whole sentence
+# print ("Today is is", day,
+#         " and your favorite holiday is ", favHoliday)
+
+#string slicing 
+word = "hello"
+print(word[1:4])
